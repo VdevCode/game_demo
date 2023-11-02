73bfff0f7ee0fdedd6f6c56b217a3f794fce3bbe
Nguyen <pysanmeo@gmail.com>
2023-11-02 22:07:42 +0700

    fix building

--- src/pages/Play/components/Mark.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Mark({mark}) {
-  return (
-    <div className='absolute z-10'>{mark}</div>
-  )
-}
-
-export default Mark
--- src/shared/components/Skeleton/index.tsx
+++
@@ -1,7 +0,0 @@
-function Skelecton() {
-  return (
-    <div className="w-full h-full bg-lightSecond dark:bg-darkSecond animate-pulse" />
-  );
-}
-
-export default Skelecton;
