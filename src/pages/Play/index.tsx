edb818a328cb8147081d7d231ed0870c5b43d524
Nguyen <pysanmeo@gmail.com>
2023-11-16 01:19:49 +0700

    Fix screen

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -17,7 +17,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-const SCREEN_W = window.innerWidth;
+const SCREEN_W = window.screen.width;
 const SCREEN_H = window.screen.height;
 const GRAVITY = 0.2;
 
